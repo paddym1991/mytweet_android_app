b17a1d24361f42a27ed10a7214fe599af5a63bcb
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-05 16:55:19 +0100

    replacing existing activities and Classes with new ones

---
+++ app/src/main/java/app/mytweet/activities/Signup.java
@@ -0,0 +1,15 @@
+package app.mytweet.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.pm.mytweet.R;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
