5c3eb764fb8f46e8b7cd0bf7c57c3f5e0c583b11
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-05 22:10:18 +0100

    On click funtionality added to Welcome

--- app/src/main/java/app/mytweet/activities/Welcome.java
+++ app/src/main/java/app/mytweet/activities/Welcome.java
@@ -1,7 +1,9 @@
 package app.mytweet.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import org.pm.mytweet.R;
 
@@ -11,5 +13,13 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void loginButtonPressed (View view) {
+        startActivity(new Intent(this, Login.class));
+    }
+
+    public void signupButtonPressed (View view) {
+        startActivity(new Intent(this, Signup.class));
     }
 }
