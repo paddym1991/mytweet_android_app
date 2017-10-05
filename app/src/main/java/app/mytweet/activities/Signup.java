27cd7248a56c4e773886c0a1333bbeeffe9295c5
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-05 22:13:54 +0100

    On click register button sends user to welcome page

--- app/src/main/java/app/mytweet/activities/Signup.java
+++ app/src/main/java/app/mytweet/activities/Signup.java
@@ -1,7 +1,9 @@
 package app.mytweet.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import org.pm.mytweet.R;
 
@@ -11,5 +13,9 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void registerButtonPressed (View view) {
+        startActivity(new Intent(this, Welcome.class));
     }
 }
