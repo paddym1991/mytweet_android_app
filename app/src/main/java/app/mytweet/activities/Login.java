fda80cebe20b8cb5ee25c1bbb0c7c4b2e1ea92d5
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-05 22:17:19 +0100

    On click signin button sends user to mytweet page

--- app/src/main/java/app/mytweet/activities/Login.java
+++ app/src/main/java/app/mytweet/activities/Login.java
@@ -1,7 +1,9 @@
 package app.mytweet.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import org.pm.mytweet.R;
 
@@ -11,5 +13,9 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signinButtonPressed (View view) {
+        startActivity(new Intent(this, MyTweetActivity.class));
     }
 }
