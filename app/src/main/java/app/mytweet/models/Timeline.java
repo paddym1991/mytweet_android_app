1cfd00be5a4c2642d9379e924b18e6696d837f0b
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-09 15:50:04 +0100

    Added Timeline model to incorporate a collection of tweets.

--- app/src/main/java/app/mytweet/models/Timeline.java
+++ app/src/main/java/app/mytweet/models/Timeline.java
@@ -1,8 +1,35 @@
 package app.mytweet.models;
 
-/**
- * Created by Paddym1991 on 07/10/2017.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.pm.mytweet.R;
+
+import java.util.ArrayList;
 
 public class Timeline {
+
+    public ArrayList<Tweet> tweets;
+
+    public Timeline() {
+        tweets = new ArrayList<>();
+    }
+
+    public void addTweet(Tweet tweet) {
+        tweets.add(tweet);
+    }
+
+    public Tweet getTweet(Long id) {
+        Log.i(getClass().getSimpleName(), "Long parameter id: " + id);
+
+        for (Tweet tweet : tweets) {
+            if (id.equals(tweet.id)) {
+                return tweet;
+            }
+        }
+        return null;
+    }
 }
+
+
