ca17a69a92b7447941004d102cb22be150bd52aa
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-12 17:03:34 +0100

    Application object (MyRentApp) updated to implement Serializer and JSON.

--- app/src/main/java/app/mytweet/app/MyTweetApp.java
+++ app/src/main/java/app/mytweet/app/MyTweetApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import static app.helpers.LogHelper.info;
 
 import app.mytweet.models.Portfolio;
+import app.mytweet.models.PortfolioSerializer;
 
 /**
  * Created by Paddym1991 on 09/10/2017.
@@ -12,11 +13,15 @@ import app.mytweet.models.Portfolio;
 public class MyTweetApp extends Application {
 
     public Portfolio portfolio;
+    //introduce a field to hold the file name we will use to store the portfolio
+    private static final String FILENAME = "portfolio.json";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        portfolio = new Portfolio();
+        //portfolio creation
+        PortfolioSerializer serializer = new PortfolioSerializer(this, FILENAME);
+        portfolio = new Portfolio(serializer);
 
         info(this, "MyTweet app launched");
     }
